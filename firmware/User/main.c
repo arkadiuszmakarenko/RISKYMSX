13427455babaaebc3fa61164351d8434ee4533f1
arkadiuszmakarenko <agnieszkaarek@gmail.com>
2025-07-10 18:54:03 +0100

    v.3.2.2 Minor tidy up. Removed onchip memory split

--- firmware/User/main.c
+++ firmware/User/main.c
@@ -13,22 +13,6 @@ int main (void) {
     SystemCoreClockUpdate();
     Delay_Init();
     GPIO_Config();
-
-    //  USART_Printf_Init (115200);
-    // printf (" SystemClk:\t%d\r\n", SystemCoreClock);
-    // printf (" ChipID:\t%08x\r\n", DBGMCU_GetCHIPID());
-
-
-    // if (!GPIO_ReadInputDataBit (GPIOC, GPIO_Pin_6)) {
-    //    PFIC->SCTLR |= (1 << 31);
-    // }
-
-    // Check memory split 5 to 7th bit needs to be 111
-    if ((OB->USER & 0b1110000) >> 4 != 0b111) {
-        // Configure 288kb Flash + 32k RAM.
-        SetSplit();
-    }
-
     Init_Cart();
 
     if (type == MSXTERMINAL) {
--- firmware/User/version.h
+++ firmware/User/version.h
@@ -2,6 +2,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define FIRMWARE_VERSION_STRING "v3.2.1"
+#define FIRMWARE_VERSION_STRING "v3.2.2"
 
 #endif  // VERSION_H
